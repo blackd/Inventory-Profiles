df35b76155a25bbb21c226629f5eaaf4cdf63d69
jsnimda <7615255+jsnimda@users.noreply.github.com>
2019-11-04 22:06:39 +0800

    Impl config button, Impl some tweaks

--- src/main/java/io/github/jsnimda/inventoryprofiles/InventoryProfiles.java
+++ src/main/java/io/github/jsnimda/inventoryprofiles/InventoryProfiles.java
@@ -2,7 +2,10 @@ package io.github.jsnimda.inventoryprofiles;
 
 import fi.dy.masa.malilib.event.InitializationHandler;
 import io.github.jsnimda.inventoryprofiles.forge.ForgeEventHandler;
+import io.github.jsnimda.inventoryprofiles.gui.GuiConfigs;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 
 /**
@@ -14,6 +17,10 @@ public class InventoryProfiles {
   public InventoryProfiles() {
 
     MinecraftForge.EVENT_BUS.register(new ForgeEventHandler());
+
+    ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.CONFIGGUIFACTORY, ()->{
+      return (x,y)->new GuiConfigs();
+    });
 
     InitializationHandler.getInstance().registerInitializationHandler(new InitHandler());
     
--- src/main/java/io/github/jsnimda/inventoryprofiles/forge/ForgeEventHandler.java
+++ src/main/java/io/github/jsnimda/inventoryprofiles/forge/ForgeEventHandler.java
@@ -1,18 +1,32 @@
 package io.github.jsnimda.inventoryprofiles.forge;
 
+import java.lang.reflect.Field;
 import java.util.List;
 
 import com.mojang.blaze3d.platform.GlStateManager;
 
+import org.apache.commons.lang3.reflect.FieldUtils;
+
+import io.github.jsnimda.inventoryprofiles.config.Configs.Tweaks;
 import io.github.jsnimda.inventoryprofiles.gui.ToolTips;
 import io.github.jsnimda.inventoryprofiles.gui.inject.GuiSortingButtons;
+import io.github.jsnimda.inventoryprofiles.sorter.SorterEventPort;
+import io.github.jsnimda.inventoryprofiles.sorter.util.Current;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screen.inventory.ContainerScreen;
 import net.minecraft.client.gui.widget.Widget;
+import net.minecraft.client.multiplayer.PlayerController;
+import net.minecraft.client.util.InputMappings;
 import net.minecraft.inventory.container.Container;
 import net.minecraftforge.client.event.GuiContainerEvent;
+import net.minecraftforge.client.event.GuiScreenEvent;
 import net.minecraftforge.client.event.GuiScreenEvent.DrawScreenEvent;
 import net.minecraftforge.client.event.GuiScreenEvent.InitGuiEvent;
+import net.minecraftforge.event.TickEvent.ClientTickEvent;
+import net.minecraftforge.event.TickEvent.Phase;
+import net.minecraftforge.event.entity.player.PlayerContainerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 
 /**
  * ForgeEventHandler
@@ -24,7 +38,7 @@ public class ForgeEventHandler {
   }
 
   @SubscribeEvent
-  public void onDrawScreenEventPost(DrawScreenEvent.Post e) { // MixinAbstractContainerScreen.render
+  public void onDrawScreenPost(DrawScreenEvent.Post e) { // MixinAbstractContainerScreen.render
     if (!ToolTips.current.isEmpty()) {
       GlStateManager.pushMatrix();
       ToolTips.renderAll();
@@ -34,7 +48,7 @@ public class ForgeEventHandler {
   }
 
   @SubscribeEvent
-  public void onInitGuiEventPost(InitGuiEvent.Post e) {
+  public void onInitGuiPost(InitGuiEvent.Post e) { // MixinAbstractContainerScreen.init
     if (e.getGui() instanceof ContainerScreen) {
       ContainerScreen<?> containerScreen = (ContainerScreen<?>) e.getGui();
       Container container = containerScreen.getContainer();
@@ -42,8 +56,54 @@ public class ForgeEventHandler {
       int top = (containerScreen.height - containerScreen.getYSize()) / 2;
       int containerWidth = containerScreen.getXSize();
       int containerHeight = containerScreen.getYSize();
-      List<Widget> buttons = GuiSortingButtons.gets(containerScreen, container, left, top, containerWidth, containerHeight);
+      List<Widget> buttons = GuiSortingButtons.gets(containerScreen, container, left, top, containerWidth,
+          containerHeight);
       buttons.forEach(x -> e.addWidget(x));
+    }
+  }
+
+  @SubscribeEvent
+  public void onGuiKeyPressedPre(GuiScreenEvent.KeyboardKeyPressedEvent.Pre e) { // Tweaks.PREVENT_CLOSE_GUI_DROP_ITEM
+    if (!Current.inGame()) return;
+    InputMappings.Input mouseKey = InputMappings.getInputByCode(e.getKeyCode(), e.getScanCode());
+    if (Tweaks.PREVENT_CLOSE_GUI_DROP_ITEM.getBooleanValue()
+        && (e.getKeyCode() == 256 || Current.MC().gameSettings.keyBindInventory.isActiveAndMatches(mouseKey))) {
+      SorterEventPort.handleCloseContainer();
+    }
+  }
+
+  PlayerController pc = null;
+
+  //blockHitDelay
+  Field blockHitDelayField = null; // field_78781_i
+
+  //rightClickDelayTimer
+  Field rightClickDelayTimerField = null; // field_71467_ac
+
+  @SubscribeEvent
+  public void onTick(ClientTickEvent e) { // Tweaks.DISABLE_BLOCK_BREAKING_COOLDOWN, Tweaks.DISABLE_ITEM_USE_COOLDOWN
+    if (!Current.inGame()) return;
+    if (e.phase != Phase.START) return;
+    if (Tweaks.DISABLE_BLOCK_BREAKING_COOLDOWN.getBooleanValue()) {
+      if (pc == null || pc != Current.interactionManager()) {
+        pc = Current.interactionManager();
+        blockHitDelayField = ObfuscationReflectionHelper.findField(PlayerController.class, "field_78781_i");
+      }
+      try {
+        FieldUtils.writeField(blockHitDelayField, pc, 0, true);
+      } catch (IllegalAccessException e2) {
+        e2.printStackTrace();
+      }
+    }
+    if (Tweaks.DISABLE_ITEM_USE_COOLDOWN.getBooleanValue()) {
+      if (rightClickDelayTimerField == null) {
+        rightClickDelayTimerField = ObfuscationReflectionHelper.findField(Minecraft.class, "field_71467_ac");
+      }
+      try {
+        FieldUtils.writeField(rightClickDelayTimerField, Current.MC(), 0, true);
+      } catch (IllegalAccessException e2) {
+        e2.printStackTrace();
+      }
     }
   }
 }
