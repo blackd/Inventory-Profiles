1efda4de1aa56c89bec9d81d2eb1900e8d578edf
jsnimda <7615255+jsnimda@users.noreply.github.com>
2020-03-02 02:14:08 +0800

    Translating 5: IConfigOptionPrimitive

---
+++ src/main/java/io/github/jsnimda/common/config/IConfigOptionPrimitive.kt
@@ -0,0 +1,46 @@
+package io.github.jsnimda.common.config
+
+import com.google.gson.JsonElement
+import com.google.gson.JsonPrimitive
+import io.github.jsnimda.common.Log
+
+interface IConfigOptionPrimitive<T> : IConfigOption {
+  var value: T
+  val defaultValue: T
+  @JvmDefault override fun isModified(): Boolean {
+    return value != defaultValue
+  }
+
+  @JvmDefault override fun resetToDefault() {
+    value = defaultValue
+  }
+
+  @JvmDefault override fun toJsonElement(): JsonElement {
+    return when (val v = value) {
+      is Boolean -> JsonPrimitive(v)
+      is Number -> JsonPrimitive(v)
+      is String -> JsonPrimitive(v)
+      else -> throw UnsupportedOperationException("Not implemented yet")
+    }
+  }
+
+  @JvmDefault override fun fromJsonElement(element: JsonElement) {
+    resetToDefault()
+    try {
+      val p = element.asJsonPrimitive
+      @Suppress("UNCHECKED_CAST")
+      value = when (val v = defaultValue) {
+        is Boolean -> p.asBoolean as T
+        is Number -> when (v) {
+          is Int -> p.asInt as T
+          is Double -> p.asDouble as T
+          else -> throw UnsupportedOperationException("Not implemented yet")
+        }
+        is String -> p.asString as T
+        else -> throw UnsupportedOperationException("Not implemented yet")
+      }
+    } catch (e: Exception) {
+      Log.warn("[invprofiles.common] Failed to set config value for '$key' from the JSON element '$element'")
+    }
+  }
+}
