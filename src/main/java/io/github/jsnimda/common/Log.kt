c190d5eb806c454f20b7d7fdd5214b20bbc7e4c8
jsnimda <7615255+jsnimda@users.noreply.github.com>
2020-03-02 00:27:39 +0800

    Add Log.kt

---
+++ src/main/java/io/github/jsnimda/common/Log.kt
@@ -0,0 +1,6 @@
+package io.github.jsnimda.common
+
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
+
+object Log : Logger by LogManager.getLogger("inventoryprofiles")
