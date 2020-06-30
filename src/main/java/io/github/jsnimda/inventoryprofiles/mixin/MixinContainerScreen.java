3854760576c3c7a6418fc0880458587a7d478772
jsnimda <7615255+jsnimda@users.noreply.github.com>
2020-06-30 11:45:11 +0800

    Fix import

--- src/main/java/io/github/jsnimda/inventoryprofiles/mixin/MixinContainerScreen.java
+++ src/main/java/io/github/jsnimda/inventoryprofiles/mixin/MixinContainerScreen.java
@@ -1,23 +1,14 @@
 package io.github.jsnimda.inventoryprofiles.mixin;
 
-import java.util.List;
-
-import com.mojang.blaze3d.platform.GlStateManager;
-
-import io.github.jsnimda.common.gui.Tooltips;
 import io.github.jsnimda.inventoryprofiles.gui.inject.ContainerScreenHandler;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.ingame.ContainerScreen;
+import net.minecraft.container.Container;
+import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import io.github.jsnimda.inventoryprofiles.gui.inject.GuiSortingButtons;
-import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.gui.screen.ingame.ContainerScreen;
-import net.minecraft.client.gui.widget.AbstractButtonWidget;
-import net.minecraft.container.Container;
-import net.minecraft.text.Text;
 
 /**
  * MixinAbstractContainerScreen
@@ -31,7 +22,7 @@ public abstract class MixinContainerScreen<T extends Container> extends Screen {
 
   @Inject(at = @At("RETURN"), method = "init()V")
   protected void init(CallbackInfo info) {
-    addButton(ContainerScreenHandler.INSTANCE.getContainerInjector((ContainerScreen)(Object)this));
+    addButton(ContainerScreenHandler.INSTANCE.getContainerInjector((ContainerScreen) (Object) this));
   }
 
 //  @Inject(at = @At("RETURN"), method = "render(IIF)V")
