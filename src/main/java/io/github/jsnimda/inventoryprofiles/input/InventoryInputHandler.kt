e710514507ec5e1474d2e8969b0d1488e5ee82a2
jsnimda <7615255+jsnimda@users.noreply.github.com>
2020-07-10 03:59:57 +0800

    Remove the list of lambda

--- src/main/java/io/github/jsnimda/inventoryprofiles/input/InventoryInputHandler.kt
+++ src/main/java/io/github/jsnimda/inventoryprofiles/input/InventoryInputHandler.kt
@@ -7,7 +7,6 @@ import io.github.jsnimda.common.vanilla.VanillaUtil
 import io.github.jsnimda.common.vanilla.alias.ContainerScreen
 import io.github.jsnimda.inventoryprofiles.config.Hotkeys
 import io.github.jsnimda.inventoryprofiles.inventory.GeneralInventoryActions
-import kotlin.reflect.KFunction0
 
 object InventoryInputHandler : IInputHandler {
 
@@ -16,24 +15,23 @@ object InventoryInputHandler : IInputHandler {
     if (Vanilla.screen() != null && Vanilla.screen() !is ContainerScreen<*>) return false
 
     with(GeneralInventoryActions) {
-      listOf<Pair<ConfigHotkey, KFunction0<Unit>>>(
-        Hotkeys.SORT_INVENTORY            /**/ to ::doSort,
-        Hotkeys.SORT_INVENTORY_IN_COLUMNS /**/ to ::doSortInColumns,
-        Hotkeys.SORT_INVENTORY_IN_ROWS    /**/ to ::doSortInRows,
-        Hotkeys.MOVE_ALL_ITEMS            /**/ to ::doMoveMatch,
-        Hotkeys.DUMP_ITEM_NBT_TO_CHAT     /**/ to ::dumpItemNbt,
-      )
-    }.forEach { (hotkey, action) ->
-      try {
-        if (hotkey.isActivated()) {
-          action()
-          return true
-        }
-      } catch (e: Throwable) {
-        e.printStackTrace()
-      }
+      return Hotkeys.SORT_INVENTORY            /**/ run ::doSort
+          || Hotkeys.SORT_INVENTORY_IN_COLUMNS /**/ run ::doSortInColumns
+          || Hotkeys.SORT_INVENTORY_IN_ROWS    /**/ run ::doSortInRows
+          || Hotkeys.MOVE_ALL_ITEMS            /**/ run ::doMoveMatch
+          || Hotkeys.DUMP_ITEM_NBT_TO_CHAT     /**/ run ::dumpItemNbt
     }
+  }
 
+  inline infix fun ConfigHotkey.run(action: () -> Unit): Boolean {
+    try {
+      if (this.isActivated()) {
+        action()
+        return true
+      }
+    } catch (e: Throwable) {
+      e.printStackTrace()
+    }
     return false
   }
 
