885b79378974bd78c27df40422b215cb962612b3
jsnimda <7615255+jsnimda@users.noreply.github.com>
2020-07-09 22:00:41 +0800

    Semi convert gradle build script to .kts

---
+++ buildSrc/src/main/kotlin/GradleProperties.kt
@@ -0,0 +1,43 @@
+// Kotlin
+const val kotlin_version = "1.4-M3"
+
+// Mod Properties
+const val mod_version = "0.3.4-pre"
+const val maven_group = "io.github.jsnimda"
+const val mod_id = "inventoryprofiles"
+
+const val minecraft_version = "1.16.1"
+
+// ============
+// mod loader
+// ============
+
+const val mod_loader = "forge"
+
+// ============
+// fabric
+// ============
+
+// Fabric Properties
+// Check these on https://modmuss50.me/fabric.html
+const val yarn_mappings = "1.16.1+build.20"
+const val loader_version = "0.8.9+build.203"
+
+const val loom_version = "0.4-SNAPSHOT"
+
+// Dependencies
+// currently not on the main fabric site, check on the maven: https://maven.fabricmc.net/net/fabricmc/fabric-api/fabric-api
+//  fabric_version=0.13.1+build.370-1.16
+// https://maven.fabricmc.net/io/github/prospector/modmenu/
+const val mod_menu_version = "1.12.2+build.16"
+
+// ============
+// forge
+// ============
+
+// http://files.minecraftforge.net/
+val forge_version = "32.0.61"
+// http://export.mcpbot.bspk.rs/
+// see mdk
+val mappings_channel = "snapshot"
+val mappings_version = "20200514-1.16"
