8569fd6d71212d5e4b3ed902d6d51ead39a5e4a9
jsnimda <7615255+jsnimda@users.noreply.github.com>
2020-07-14 02:40:30 +0800

    Add exclude META-INF/maven

--- build.gradle.kts
+++ build.gradle.kts
@@ -150,6 +150,7 @@ tasks.shadowJar {
   exclude("**/*_ws.class") // fixme find a better solution for removing *.ws.kts
   exclude("**/*_ws$*.class")
   exclude("mappings/mappings.tiny") // before kt, build .jar don"t have this folder (this 500K thing)
+  exclude("META-INF/maven/**")
 }
 
 val proguard by tasks.registering(ProGuardTask::class) {
