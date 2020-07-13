6699ea211463079b18928cf2aab05bc56089a038
jsnimda <7615255+jsnimda@users.noreply.github.com>
2020-07-14 05:51:39 +0800

    Fix vanilla <-> my widget event

--- src/main/java/io/github/jsnimda/inventoryprofiles/gui/inject/AsVanillaWidget.kt
+++ src/main/java/io/github/jsnimda/inventoryprofiles/gui/inject/AsVanillaWidget.kt
@@ -69,45 +69,51 @@ class AsVanillaWidget() : AbstractButtonWidget(0, 0, rScreenWidth, rScreenHeight
 
  */
 
-  // copy and paste form BaseScreen
+  // render
   override fun func_230430_a_(matrixStack: MatrixStack?, i: Int, j: Int, f: Float) {
 //  rMatrixStack = matrixStack ?: MatrixStack().also { Log.debug("null matrixStack") }
 //  render(i, j, f)
   }
 
-  //  override fun mouseClicked(d: Double, e: Double, i: Int): Boolean =
-  open fun mouseClicked(d: Double, e: Double, i: Int): Boolean = func_231044_a_(d, e, i)
-  override fun func_231044_a_(d: Double, e: Double, i: Int): Boolean =
+  // copy and paste form BaseScreen
+  open fun mouseClicked(d: Double, e: Double, i: Int): Boolean =
     rootWidget.mouseClicked(d.toInt(), e.toInt(), i)
 
-  //  override fun mouseReleased(d: Double, e: Double, i: Int): Boolean =
-  open fun mouseReleased(d: Double, e: Double, i: Int): Boolean = mouseReleased(d, e, i)
-  override fun func_231048_c_(d: Double, e: Double, i: Int): Boolean =
+  open fun mouseReleased(d: Double, e: Double, i: Int): Boolean =
     rootWidget.mouseReleased(d.toInt(), e.toInt(), i)
 
-  //  override fun mouseDragged(d: Double, e: Double, i: Int, f: Double, g: Double): Boolean =
-  open fun mouseDragged(d: Double, e: Double, i: Int, f: Double, g: Double): Boolean = func_231045_a_(d, e, i, f, g)
-  override fun func_231045_a_(d: Double, e: Double, i: Int, f: Double, g: Double): Boolean =
+  open fun mouseDragged(d: Double, e: Double, i: Int, f: Double, g: Double): Boolean =
     rootWidget.mouseDragged(d, e, i, f, g) // fixme fix dx dy decimal rounding off
 
-  //  override fun mouseScrolled(d: Double, e: Double, f: Double): Boolean =
-  open fun mouseScrolled(d: Double, e: Double, f: Double): Boolean = func_231043_a_(d, e, f)
-  override fun func_231043_a_(d: Double, e: Double, f: Double): Boolean =
+  open fun mouseScrolled(d: Double, e: Double, f: Double): Boolean =
     rootWidget.mouseScrolled(d.toInt(), e.toInt(), f)
 
-  //  override fun keyPressed(keyCode: Int, scanCode: Int, modifiers: Int): Boolean =
   open fun keyPressed(keyCode: Int, scanCode: Int, modifiers: Int): Boolean =
-    func_231046_a_(keyCode, scanCode, modifiers)
-
-  override fun func_231046_a_(keyCode: Int, scanCode: Int, modifiers: Int): Boolean =
 //    super.keyPressed(keyCode, scanCode, modifiers) || rootWidget.keyPressed(keyCode, scanCode, modifiers)
     super.func_231046_a_(keyCode, scanCode, modifiers) || rootWidget.keyPressed(keyCode, scanCode, modifiers)
 
   override fun keyReleased(keyCode: Int, scanCode: Int, modifiers: Int): Boolean =
     rootWidget.keyReleased(keyCode, scanCode, modifiers)
 
-  //  override fun charTyped(charIn: Char, modifiers: Int): Boolean =
-  open fun charTyped(charIn: Char, modifiers: Int): Boolean = func_231042_a_(charIn, modifiers)
-  override fun func_231042_a_(charIn: Char, modifiers: Int): Boolean =
+  open fun charTyped(charIn: Char, modifiers: Int): Boolean =
     rootWidget.charTyped(charIn, modifiers)
+
+  // ~.~
+  override fun func_231044_a_(d: Double, e: Double, i: Int): Boolean =
+    mouseClicked(d, e, i)
+
+  override fun func_231048_c_(d: Double, e: Double, i: Int): Boolean =
+    mouseReleased(d, e, i)
+
+  override fun func_231045_a_(d: Double, e: Double, i: Int, f: Double, g: Double): Boolean =
+    mouseDragged(d, e, i, f, g)
+
+  override fun func_231043_a_(d: Double, e: Double, f: Double): Boolean =
+    mouseScrolled(d, e, f)
+
+  override fun func_231046_a_(keyCode: Int, scanCode: Int, modifiers: Int): Boolean =
+    keyPressed(keyCode, scanCode, modifiers)
+
+  override fun func_231042_a_(charIn: Char, modifiers: Int): Boolean =
+    charTyped(charIn, modifiers)
 }
