7043fc1fc269deffa141c35b2246a9d3ede46d8f
jsnimda <7615255+jsnimda@users.noreply.github.com>
2020-07-14 06:06:36 +0800

    Disable DebugFunc for forge

--- src/main/java/io/github/jsnimda/inventoryprofiles/debug/DebugFunc.kt
+++ src/main/java/io/github/jsnimda/inventoryprofiles/debug/DebugFunc.kt
@@ -1,25 +1,19 @@
 package io.github.jsnimda.inventoryprofiles.debug
 
-import io.github.jsnimda.common.util.trySwallow
-import io.github.jsnimda.common.util.usefulName
-import net.minecraft.network.NetworkSide
-import net.minecraft.network.NetworkState
-import net.minecraft.network.Packet
-
 object DebugFunc {
   fun dumpPacketId() {
-    dump(NetworkSide.SERVERBOUND)
-    dump(NetworkSide.CLIENTBOUND)
+//    dump(NetworkSide.SERVERBOUND)
+//    dump(NetworkSide.CLIENTBOUND)
   }
 
-  private fun dump(side: NetworkSide) {
-    println(side)
-    var packet: Packet<*>
-    for (i in 0..5000) {
-      packet = trySwallow { NetworkState.PLAY.getPacketHandler(side, i) } ?: return
-      println("$i ${packet.javaClass.usefulName}")
-    }
-  }
+//  private fun dump(side: NetworkSide) {
+//    println(side)
+//    var packet: Packet<*>
+//    for (i in 0..5000) {
+//      packet = trySwallow { NetworkState.PLAY.getPacketHandler(side, i) } ?: return
+//      println("$i ${packet.javaClass.usefulName}")
+//    }
+//  }
 
   // see ServerPlayNetworkHandler onClickWindow line 1202
 }
