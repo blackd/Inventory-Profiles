cd5cd9ad1fbbf55c8f0e7179980657df81611ab9
Victor B <victor.bonnelle@protonmail.com>
2021-08-01 22:07:21 +0200

    Improve description

--- description/build_html.py
+++ description/build_html.py
@@ -2,36 +2,52 @@
 #-*- coding: utf-8 -*-
 
 import pypandoc
-from premailer import transform
-
-def makeStage1():
-    stage1 = open("stage1.html", "wb")
-    converted = pypandoc.convert_file("description.md",
-                                      "html",
-                                      format="md",
-                                      filters=["pandoc-include"]
-    )
-    stage1.write(converted.encode("utf-8"))
-
+from premailer import transform as premail
+from pathlib import Path
+import re
 
 def main():
-    with open("output.html", "wb") as output:
-        output.write(
-            transform(
-                pypandoc.convert_file(
-                    "description.md",
-                    "html",
-                    format="md",
-                    filters=["pandoc-include"]
-                ),
-                allow_loading_external_files=True,
-                external_styles=["style.css"],
-                remove_classes=False
-            )
-            .replace("<html><head></head><body>", "")
-            .replace("</body></html>", "")
-            .encode("utf-8")
+    Path("out/debug").mkdir(parents=True, exist_ok=True)
+
+    html = pypandoc.convert_file(
+        "description.md",
+        "html",
+        format="md",
+        filters=["pandoc-include"]
+    )
+
+    with open("out/debug/pandoc.html", "wb") as f:
+        f.write(html.encode("utf-8"))
+
+    premailed = premail(
+        html,
+        allow_loading_external_files=True,
+        external_styles=["style.css"],
+        remove_classes=False
+    )
+
+    with open("out/debug/premail.html", "wb") as f:
+        f.write(premailed.encode("utf-8"))
+
+    output = (
+        re.sub(
+            "<!-- .+ -->\n",
+            "",
+            premailed
         )
+        .replace("<html><head></head><body>", "")
+        .replace("</body></html>", "")
+        .replace("<h1", "<p></p>\n<h1")
+        .replace("<h2", "<p></p>\n<h2")
+        .replace("<h3", "<p></p>\n<h3")
+        .replace("</h1>", "</h1>\n<p></p>")
+        .replace("</h2>", "</h2>\n<p></p>")
+        .replace("</h3>", "</h3>\n<p></p>")
+        .replace("<p></p>\n<p></p>", "<p></p>")
+    )
+
+    with open("out/output.html", "wb") as f:
+        f.write(output.encode("utf-8"))
 
 if __name__ == "__main__":
     main()
