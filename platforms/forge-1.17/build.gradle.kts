7b389a7710db850bbb58e218680165f01f014ff1
Plamen K. Kosseff <p.kosseff@gmail.com>
2021-08-16 23:47:21 +0300

    update to latest 1.17 forge first time they didn't break everything :)

--- platforms/forge-1.17/build.gradle.kts
+++ platforms/forge-1.17/build.gradle.kts
@@ -11,7 +11,7 @@ val supported_minecraft_versions = listOf("1.17.1")
 val mod_loader = "forge"
 val mod_version = project.version
 val minecraft_version = "1.17.1"
-val forge_version = "37.0.29"
+val forge_version = "37.0.34"
 
 
 logger.lifecycle("""
