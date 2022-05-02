2288381964906d21eb6020c4ba352a33a4e6f134
Plamen K. Kosseff <p.kosseff@gmail.com>
2022-05-02 22:45:01 +0300

    ....

--- do-build.sh
+++ do-build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-./gradlew --no-daemon --no-parallel --max-workers 2 -S clean build &
+./gradlew --no-daemon --no-parallel --max-workers 1 -S  --foreground build &
 
 wait $(jobs -p)
+
+./gradlew --no-daemon --no-parallel --max-workers 1 -S  --foreground build &
 
 exit 0
