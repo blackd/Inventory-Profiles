b27045a8383aa5ade41d7aa4e5dec29c9c2879c1
Plamen K. Kosseff <p.kosseff@gmail.com>
2022-10-28 17:22:32 +0300

    updating libIPN dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -87,7 +87,7 @@ allprojects {
     version = versionObj.toString()
     group = "org.anti-ad.mc"
     ext.set("mod_artefact_version", versionObj.toCleanString())
-    ext.set("libIPN_version", "1.0.4-SNAPSHOT")
+    ext.set("libIPN_version", "1.0.4")
 
     tasks.withType<JavaCompile>().configureEach {
         options.isFork = true
