f87449785d79bca785e42c4bb6633c9930da8811
Plamen K. Kosseff <p.kosseff@gmail.com>
2023-07-02 14:16:41 +0300

    removing 1.16 and 1.19.4

--- platforms/fabric-1.16/setup_build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-export ORG_GRADLE_PROJECT_signingKey=`cat ~/github-stuff/'Plamen K. Kosseff_0xC9ABD4CD_SECRET-2.asc'`
-export ORG_GRADLE_PROJECT_signingPassword='X7tS9R$MjqKuM`LoVmT`~cpr'
-
-export ORG_GRADLE_PROJECT_sonatypeUsername=q7nMEKIv
-export ORG_GRADLE_PROJECT_sonatypePassword=GY4qldKaP+he8GnjhduUB2ze1FdRjrOfDV3RtKSJ6Um7
-
