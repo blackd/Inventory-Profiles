22d9f3b73b077b4cfb8584952fbfc0e1b3aff4aa
Plamen K. Kosseff <p.kosseff@gmail.com>
2025-02-07 18:25:06 +0200

    Downgrade to compiling with 2.0.x

--- build.gradle.kts
+++ build.gradle.kts
@@ -48,8 +48,8 @@ dependencies {
 
 plugins {
     //`kotlin-dsl`
-    kotlin("jvm") version "2.1.10"
-    kotlin("plugin.serialization") version "2.1.10"
+    kotlin("jvm") version "2.0.21"
+    kotlin("plugin.serialization") version "2.0.21"
     idea
     `java-library`
     `maven-publish`
@@ -76,7 +76,7 @@ allprojects {
     group = "org.anti-ad.mc"
     ext.set("mod_artefact_version", versionObj.toCleanString())
     ext.set("mod_artefact_is_release", versionObj.isRelease())
-    ext.set("libIPN_version", "6.3.0")
+    ext.set("libIPN_version", "6.3.1")
     ext.set("libIPN_version_max", "6.4")
 
     tasks.withType<JavaCompile>().configureEach {
